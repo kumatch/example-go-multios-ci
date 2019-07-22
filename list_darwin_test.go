f6ca9f47984bb9b436364eef16eb75d31d14de15
Yosuke Kumakura <kumatch@gmail.com>
2019-07-23 00:00:33 +0900

    added.

---
+++ list_darwin_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestOutputFilesOnDarwin(t *testing.T) {
+	out, err := outputFiles("files")
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	expects := `.
+..
+a.txt
+b.txt
+c.txt`
+
+	if expects != out {
+		t.Errorf("want: %s\ngot: %s", expects, out)
+	}
+}
