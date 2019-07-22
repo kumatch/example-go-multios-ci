140c5fde783461215c6a2cc35ed312730fcc3dee
Yosuke Kumakura <kumatch@gmail.com>
2019-07-22 23:59:30 +0900

    fixed windows test.

--- list_windows_test.go
+++ list_windows_test.go
@@ -2,14 +2,14 @@ package main
 
 import "testing"
 
-func TestOutputFiles(t *testing.T) {
+func TestOutputFilesOnWindows(t *testing.T) {
 	out, err := outputFiles("files")
 	if err != nil {
 		t.Error(err)
 		return
 	}
 
-	expects := "a.txt\r\nb.txt\r\n\r\nc.txt\r\nd.txt" // failed
+	expects := "a.txt\r\nb.txt\r\nc.txt"
 
 	if expects != out {
 		t.Errorf("want: %s\r\ngot: %s", expects, out)
